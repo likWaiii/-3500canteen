406cd5f21ffe4835505f72cd2d9b492a1beb691d
gigtydog <15764291630@163.com>
2025-05-12 10:39:34 +0800

    柜台交互模式已从角色靠近改为鼠标悬停

--- -3500_canteen/Assets/Scripts/Player.cs
+++ -3500_canteen/Assets/Scripts/Player.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, IKithenObjectParent {
+public class Player : MonoBehaviour, IKithenObjectParent
+{
 
     public static Player Instance { get; private set; }
 
@@ -11,7 +12,8 @@ public class Player : MonoBehaviour, IKithenObjectParent {
 
     public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
 
-    public class OnSelectedCounterChangedEventArgs : EventArgs {
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
         public BaseCounter selectedCounter;
     }
 
@@ -27,72 +29,97 @@ public class Player : MonoBehaviour, IKithenObjectParent {
     private BaseCounter selectedCounter;
     private KitchenObject kitchenObject;
 
-    private void Awake() {
+    private void Awake()
+    {
 
-        if (Instance != null) {
+        if (Instance != null)
+        {
             Debug.LogError("Something Went Wrong there are more than one player");
         }
         Instance = this;
     }
 
-    private void Start() {
+    private void Start()
+    {
         gameInput.OnInteractActions += GameInput_OnInteractActions;
         gameInput.OnInteractAlternateActions += GameInput_OnInteractAlternateActions;
     }
 
-    private void GameInput_OnInteractAlternateActions(object sender, EventArgs e) {
+    private void GameInput_OnInteractAlternateActions(object sender, EventArgs e)
+    {
         if (!KitchenGameManager.Instance.IsGamePlaying()) return;
-        if (selectedCounter != null) {
+        if (selectedCounter != null)
+        {
             selectedCounter.InteractAlternate(this);
         }
     }
 
-    private void GameInput_OnInteractActions(object sender, System.EventArgs e) {
+    private void GameInput_OnInteractActions(object sender, System.EventArgs e)
+    {
         if (!KitchenGameManager.Instance.IsGamePlaying()) return;
-        if (selectedCounter != null) {
+        if (selectedCounter != null)
+        {
             selectedCounter.Interact(this);
         }
     }
 
-    private void Update() {
+    private void Update()
+    {
         HandleMovement();
         HandleInteractions();
 
     }
 
-    public bool IsWalking() {
+    public bool IsWalking()
+    {
         return isWalking;
     }
 
-    private void HandleInteractions() {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero) {
-            lastInteractDir = moveDir;
+    private void HandleInteractions()
+    {
+        // 获取主相机
+        Camera mainCamera = Camera.main;
+        if (mainCamera == null)
+        {
+            Debug.LogError("主相机未找到！");
+            return;
         }
 
-        float interactDis = 2f;
+        // 从相机通过鼠标位置发射射线
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDis, counterLayerMask)) {
-            if (raycastHit.transform.TryGetComponent(out BaseCounter baseCounter)) {
-                //Has Clear Counter
-                if (baseCounter != selectedCounter) {
+        float interactDis = 1000f;
+
+        // 进行射线检测
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, interactDis, counterLayerMask))
+        {
+           
+            // 尝试获取击中物体的BaseCounter组件
+            if (raycastHit.transform.TryGetComponent(out BaseCounter baseCounter))
+            {
+                // 如果BaseCounter组件不为当前选中的
+                if (baseCounter != selectedCounter)
+                {
                     SetSelectedCounter(baseCounter);
                 }
             }
-            else {
+            else
+            {
+                // 击中的物体没有BaseCounter组件，设置为null
                 SetSelectedCounter(null);
-
+                Debug.LogWarning("击中的物体没有BaseCounter组件！");
             }
         }
-        else {
+        else
+        {
+            // 射线未击中任何物体，设置为null
             SetSelectedCounter(null);
+            Debug.LogWarning("射线未击中任何物体！");
         }
     }
 
-    private void HandleMovement() {
+    private void HandleMovement()
+    {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -102,7 +129,8 @@ public class Player : MonoBehaviour, IKithenObjectParent {
         float moveDistance = speed * Time.deltaTime;
         canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
-        if (!canMove) {
+        if (!canMove)
+        {
             // Can't move on movDir
 
 
@@ -110,10 +138,12 @@ public class Player : MonoBehaviour, IKithenObjectParent {
             Vector3 movDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
             canMove = (moveDir.x < -.5f || moveDir.x > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDirX, moveDistance);
 
-            if (canMove) {
+            if (canMove)
+            {
                 moveDir = movDirX;
             }
-            else {
+            else
+            {
 
                 // Can't move 
 
@@ -121,17 +151,20 @@ public class Player : MonoBehaviour, IKithenObjectParent {
                 Vector3 movDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
                 canMove = (moveDir.z < -.5f || moveDir.z > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDirZ, moveDistance);
 
-                if (canMove) {
+                if (canMove)
+                {
                     moveDir = movDirZ;
                 }
-                else {
+                else
+                {
                     //Can't movee
                 }
             }
 
         }
 
-        if (canMove) {
+        if (canMove)
+        {
             transform.position += moveDir * moveDistance;
         }
 
@@ -142,37 +175,45 @@ public class Player : MonoBehaviour, IKithenObjectParent {
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
-    private void SetSelectedCounter(BaseCounter selectedCounter) {
+    private void SetSelectedCounter(BaseCounter selectedCounter)
+    {
         this.selectedCounter = selectedCounter;
 
-        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+        {
             selectedCounter = selectedCounter
         });
 
     }
 
     // Interfaces functions 
-    public Transform GetKitchenObjectFollowTransform() {
+    public Transform GetKitchenObjectFollowTransform()
+    {
         return kitchenObjectHoldPoint;
     }
 
-    public void SetKitchenObject(KitchenObject kitchenObject) {
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
         this.kitchenObject = kitchenObject;
 
-        if (kitchenObject != null) {
+        if (kitchenObject != null)
+        {
             OnPickedSomething?.Invoke(this, EventArgs.Empty);
         }
     }
 
-    public KitchenObject GetKitchenObject() {
+    public KitchenObject GetKitchenObject()
+    {
         return kitchenObject;
     }
 
-    public void ClearKitchenObject() {
+    public void ClearKitchenObject()
+    {
         kitchenObject = null;
     }
 
-    public bool HasKitchenObject() {
+    public bool HasKitchenObject()
+    {
         return kitchenObject != null;
     }
 
