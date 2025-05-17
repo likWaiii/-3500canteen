7215f201c0b642df796dc984f1b54dcb39d03686
gigtydog <15764291630@163.com>
2025-05-17 21:58:29 +0800

    翻面煎锅

--- -3500_canteen/Assets/Scripts/StoveCounterVisual.cs
+++ -3500_canteen/Assets/Scripts/StoveCounterVisual.cs
@@ -16,7 +16,7 @@ public class StoveCounterVisual : MonoBehaviour {
     }
 
     private void StoveCounter_OnStateChanged(object sender, SStoveCounter.OnStateChangedEventArgs e) {
-        bool showVisual = e.state == SStoveCounter.State.Frying || e.state == SStoveCounter.State.Fried;
+        bool showVisual = e.state == SStoveCounter.State.Frying1 || e.state == SStoveCounter.State.Fried;
         stoveOnGameObject.SetActive(showVisual);//аёвс
         particlesOnGameObject.SetActive(showVisual);//ялнМ
     }
