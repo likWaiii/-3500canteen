26e6278f757c46855dc863a4ec8295b6081e7c46
王浩东 <37220232203832@stu.xmu.edu.cn>
2025-05-18 17:05:56 +0800

    增加单个kitchenobject价值和总价值得分，修改happy表情延迟

--- -3500_canteen/Assets/Scripts/KitchenObjectOS.cs
+++ -3500_canteen/Assets/Scripts/KitchenObjectOS.cs
@@ -9,5 +9,5 @@ public class KitchenObjectOS : ScriptableObject
     public Transform prefab;
     public Sprite sprite;
     public string objectName;
-
+    public int value;// 新增字段：价值
 }
---
+++ -3500_canteen/Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        // 监听订单成功事件
+        DeliveryManager.Instance.OnRecipeSucess += OnRecipeSucess;
+        RefreshScore();
+    }
+
+    private void OnDestroy()
+    {
+        DeliveryManager.Instance.OnRecipeSucess -= OnRecipeSucess;
+    }
+
+    private void OnRecipeSucess(object sender, System.EventArgs e)
+    {
+        RefreshScore();
+    }
+
+    private void RefreshScore()
+    {
+        int value = DeliveryManager.Instance.GetTotalEarnedValue();
+        scoreText.text = $"总得分：{value}";
+    }
+}
