e7a3c478011b6f8d0c22dae8a0e9be12641c8b44
hgcode1130 <2058223294@qq.com>
2025-05-19 23:51:34 +0800

    新增了结束游戏的逻辑，我目前设置的阈值是1，所以很快结束你们可以在这个deliver Manger的Inspector来改，UI我随便让ai写的，在Canvas下面。

--- -3500_canteen/Assets/Scripts/KitchenGameManager.cs
+++ -3500_canteen/Assets/Scripts/KitchenGameManager.cs
@@ -90,6 +90,27 @@ public class KitchenGameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 游戏结束时由外部脚本（DeliveryManager、Timer 等）调用
+    /// </summary>
+    public void GameOver(bool playerWon)
+    {
+        Debug.Log("调用 GameOver，胜利？" + playerWon);
+
+        // 暂停游戏（可选）
+        Time.timeScale = 0f;
+
+        // 显示结束面板
+        if (EndGameUI.Instance != null)
+        {
+            EndGameUI.Instance.ShowResult(playerWon);
+        }
+        else
+        {
+            Debug.LogError("找不到 EndGameUI.Instance，请确认场景中已挂载 EndGameUI 脚本！");
+        }
+    }
+
     public bool IsGamePlaying()
     {
         return state == State.GamePlaying;
@@ -133,163 +154,3 @@ public class KitchenGameManager : MonoBehaviour
         }
     }
 }
-// using System;
-// using Unity.Netcode;
-// using UnityEngine;
-
-// public class KitchenGameManager : NetworkBehaviour
-// {
-//     public static KitchenGameManager Instance { get; private set; }
-
-//     public event EventHandler OnStateChanged;
-//     public event EventHandler OnGamePaused;
-//     public event EventHandler OnGameUnpaused;
-
-//     [SerializeField]
-//     private float gamePlayingTimerMax = 180f;
-
-//     private NetworkVariable<State> state = new NetworkVariable<State>(State.WaitingToStart);
-//     private NetworkVariable<float> countdownToStartTimer = new NetworkVariable<float>(3f);
-//     private NetworkVariable<float> gamePlayingTimer = new NetworkVariable<float>();
-//     private bool isGamePaused = false;
-
-//     // 分别跟踪每个玩家的得分
-//     private NetworkVariable<int> player1Score = new NetworkVariable<int>(0);
-//     private NetworkVariable<int> player2Score = new NetworkVariable<int>(0);
-
-//     private enum State
-//     {
-//         WaitingToStart,
-//         CountdownToStart,
-//         GamePlaying,
-//         GameOver,
-//     }
-
-//     private void Awake()
-//     {
-//         Instance = this;
-//     }
-
-//     public override void OnNetworkSpawn()
-//     {
-//         if (IsServer)
-//         {
-//             // 初始化网络变量
-//             state.Value = State.WaitingToStart;
-//             countdownToStartTimer.Value = 3f;
-//             gamePlayingTimer.Value = gamePlayingTimerMax;
-
-//             // 监听玩家连接
-//             NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
-//         }
-
-//         // 订阅网络变量变化事件
-//         state.OnValueChanged += OnStateValueChanged;
-//     }
-
-//     private void OnClientConnectedCallback(ulong clientId)
-//     {
-//         // 当两个玩家都连接上时，开始游戏倒计时
-//         if (NetworkManager.Singleton.ConnectedClientsIds.Count >= 2)
-//         {
-//             state.Value = State.CountdownToStart;
-//         }
-//     }
-
-//     private void OnStateValueChanged(State previousValue, State newValue)
-//     {
-//         OnStateChanged?.Invoke(this, EventArgs.Empty);
-//     }
-
-//     private void Update()
-//     {
-//         if (!IsServer)
-//             return;
-
-//         switch (state.Value)
-//         {
-//             case State.WaitingToStart:
-//                 // 等待玩家连接
-//                 break;
-
-//             case State.CountdownToStart:
-//                 countdownToStartTimer.Value -= Time.deltaTime;
-//                 if (countdownToStartTimer.Value < 0f)
-//                 {
-//                     state.Value = State.GamePlaying;
-//                     gamePlayingTimer.Value = gamePlayingTimerMax;
-//                 }
-//                 break;
-
-//             case State.GamePlaying:
-//                 gamePlayingTimer.Value -= Time.deltaTime;
-//                 if (gamePlayingTimer.Value < 0f)
-//                 {
-//                     state.Value = State.GameOver;
-//                 }
-//                 break;
-
-//             case State.GameOver:
-//                 // 游戏结束逻辑
-//                 break;
-//         }
-//     }
-
-//     public bool IsGamePlaying()
-//     {
-//         return state.Value == State.GamePlaying;
-//     }
-
-//     public bool IsGameOver()
-//     {
-//         return state.Value == State.GameOver;
-//     }
-
-//     public bool IsCountdownToStartActive()
-//     {
-//         return state.Value == State.CountdownToStart;
-//     }
-
-//     public float GetCountdownToStartTimer()
-//     {
-//         return countdownToStartTimer.Value;
-//     }
-
-//     public float GetGamePlayingTimerNormalized()
-//     {
-//         return 1 - (gamePlayingTimer.Value / gamePlayingTimerMax);
-//     }
-
-//     [ServerRpc(RequireOwnership = false)]
-//     public void AddScoreServerRpc(ulong playerId, int scoreToAdd)
-//     {
-//         if (playerId == NetworkManager.Singleton.ConnectedClientsIds[0])
-//         {
-//             player1Score.Value += scoreToAdd;
-//         }
-//         else
-//         {
-//             player2Score.Value += scoreToAdd;
-//         }
-//     }
-
-//     public int GetPlayerScore(bool isPlayer1)
-//     {
-//         return isPlayer1 ? player1Score.Value : player2Score.Value;
-//     }
-
-//     public void TogglePause()
-//     {
-//         isGamePaused = !isGamePaused;
-//         if (isGamePaused)
-//         {
-//             Time.timeScale = 0f;
-//             OnGamePaused?.Invoke(this, EventArgs.Empty);
-//         }
-//         else
-//         {
-//             Time.timeScale = 1f;
-//             OnGameUnpaused?.Invoke(this, EventArgs.Empty);
-//         }
-//     }
-// }
